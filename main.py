2e84e3371d07e237a5227e068e5a6e155dc46e81
Tonomoshia <tonomoshia@gmail.com>
2019-10-02 14:30:19 -0400

    finished user signup
    removed superfluous functions and turned them into if statements under one function. added welcome route. removed base template as not listed in requirements. added errors to index.html.

--- main.py
+++ main.py
@@ -1,38 +1,74 @@
 from flask import Flask, request, redirect, render_template
+import cgi
 import os
 import jinja2
 
 app = Flask (__name__)
-
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['POST'])
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-def username_sign_up():
+@app.route("/index", methods=['GET', 'POST'])
+def validateInputs():
     username = request.form['username']
-    if " " in username OR len(username) < 3 OR len(username) > 20:
-        error = "That's not a valid username".format(username)
-        return redirect("/?error=" + error)
-    return render_template('/,', username=username)
-
-def password_sign_up():
-    password = request.form['passowrd']
+    password = request.form['password']
     verify = request.form['verify']
+    email = request.form['email']
+    # username = ''
+    # pasword = ''
+    # verify = ''
+    # email = ''
+    # title = 'User Signup'
+    username_error = ''
+    password_error = ''
+    verify_error =''
+    email_error = ''
+
+    if int(len(username)) <=0:
+        username_error = "That's not a valid username"
+        username = ''
+    else:
+        if int(len(username)) <3 or int(len(username)) > 20:
+            username_error = "That's not a valid username"
+            username = ''
+
+    if int(len(password)) <=0:
+        password_error = "That's not a valid password"
+        password = ''
+    else:
+        if int(len(password)) < 3 or int(len(password)) >20:
+            password_error= "That's not a valid password"
+            password= ''
 
     if password != verify:
-        error = "Passwords don't match".format(verify)
-        return redirect("/?error=" + error)
-    if " " in password OR len(password) < 3 OR len(password) > 20:
-        error = "That's not a valid password".format(password)
-        return redirect("/?error=" + error)
+        verify_error = "Passwords do not match"
+        verify = ''
+    else:
+        if int(len(verify)) <=0:
+            verify_error = "Passwords do not match"
+            verify = ''
 
-def email_sign_up():
-    email = request.form['email']
-    if len(email) < 3 OR len(email) > 20:
-        error = "That's not a valid email".format(email)
-        return redirect("/?error=" + error)
-    if 
+    if int(len(email)) > 0:
+        if "@" not in email and "." not in email and " " in email:
+            email_error = "That's not a valid email"
+            email = ''
+        else:
+            if int(len(email)) < 3 or int(len(email)) > 20:
+                email_error = "That's not a valid email"
+                email = ''
 
+    if not username_error and not verify_error and not password_error and not email_error:
+        return redirect('/welcome?username={0}'.format(username))
+    else:
+        return render_template ('index.html', username=username, password=password, verify=verify, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error,  email_error=email_error)
+
+
+@app.route('/welcome', methods=['GET', 'POST'])
+def welcome():
+    username = request.form.get('username')
+    return render_template('welcome.html', username=username)
 
 if __name__ == "__main__":
     app.run()

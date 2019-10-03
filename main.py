e0346f6a02f16cec12a66109e43ac4c0fbc02f61
Tonomoshia <tonomoshia@gmail.com>
2019-10-03 19:33:02 -0400

    update email conditional

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import cgi
 import os
 import jinja2
 
-app = Flask (__name__)
+app = Flask(__name__)
 app.config['DEBUG'] = True
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/index", methods=['GET', 'POST'])
 def validateInputs():
@@ -23,35 +25,41 @@ def validateInputs():
     # title = 'User Signup'
     username_error = ''
     password_error = ''
-    verify_error =''
+    verify_error = ''
     email_error = ''
 
-    if int(len(username)) <=0:
+    if int(len(username)) <= 0:
         username_error = "That's not a valid username"
         username = ''
     else:
-        if int(len(username)) <3 or int(len(username)) > 20:
+        if int(len(username)) < 3 or int(len(username)) > 20:
             username_error = "That's not a valid username"
             username = ''
 
-    if int(len(password)) <=0:
+    if int(len(password)) <= 0:
         password_error = "That's not a valid password"
         password = ''
     else:
-        if int(len(password)) < 3 or int(len(password)) >20:
-            password_error= "That's not a valid password"
-            password= ''
+        if int(len(password)) < 3 or int(len(password)) > 20:
+            password_error = "That's not a valid password"
+            password = ''
 
     if password != verify:
         verify_error = "Passwords do not match"
         verify = ''
     else:
-        if int(len(verify)) <=0:
+        if int(len(verify)) <= 0:
             verify_error = "Passwords do not match"
             verify = ''
 
     if int(len(email)) > 0:
-        if "@" not in email and "." not in email and " " in email:
+        if "@" not in email:
+            email_error = "That's not a valid email"
+            email = ''
+        elif "." not in email:
+            email_error = "That's not a valid email"
+            email = ''
+        elif "" in email:
             email_error = "That's not a valid email"
             email = ''
         else:
@@ -62,13 +70,13 @@ def validateInputs():
     if not username_error and not verify_error and not password_error and not email_error:
         return redirect('/welcome?username={0}'.format(username))
     else:
-        return render_template ('index.html', username=username, password=password, verify=verify, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error,  email_error=email_error)
-
+        return render_template('index.html', username=username, password=password, verify=verify, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error,  email_error=email_error)
 
 @app.route('/welcome', methods=['GET', 'POST'])
 def welcome():
     username = request.form.get('username')
     return render_template('welcome.html', username=username)
+
 
 if __name__ == "__main__":
     app.run()
